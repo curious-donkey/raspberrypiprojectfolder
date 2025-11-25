3a9626858d980780493f0eea21cba00f00317c3a
curious-donkey <nathanielnarofsky@gmail.com>
2025-11-24 18:18:24 -0600

    initial items

---
+++ toycarcontroller/controllerapplication/src/main/java/nathanielnarofsky/todolist/controllerapplication/ControllerapplicationApplication.java
@@ -0,0 +1,13 @@
+package nathanielnarofsky.todolist.controllerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerapplicationApplication.class, args);
+    }
+
+}
---
+++ toycarcontroller/controllerapplication/src/test/java/nathanielnarofsky/todolist/controllerapplication/ControllerapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package nathanielnarofsky.todolist.controllerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
